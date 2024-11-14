5dd99e25e3d5cfccf7b55f2b9405b00695df8f07
chen-jacques <jacques.chen@edu.esiee.fr>
2024-11-14 09:08:52 +0000

    verifie si string palindrome

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-#### Fonction secondaire
+"""vérifie si un string est un palindrome"""
 
+def char_spec(p):
+    """enleve les accents et characteres speciaux"""
+    p = p.lower()
+    char_list = str.maketrans("éèêëàâäîïùûüçôö", "eeeeaaaiiuuucoo")
+    p=p.translate({ord(i): None for i in ' ,.;?!:-\''})
+    return p.translate(char_list)
 
 def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
+    """renvoie si le string est un palindrome"""
+    p=char_spec(p)
+    return p==p[::-1]
 
 #### Fonction principale
 
 
 def main():
-
+    """mian"""
     # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
